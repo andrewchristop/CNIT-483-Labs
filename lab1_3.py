4ecbc763fe4215e22677d80d7eb66d8db11aa752
Christopher Andrew <christopherandrew@Christophers-MacBook-Pro.local>
2023-09-18 22:45:01 -0400

    Finished 3rd problem, tweaked stack input param for lab1_2.py, and removed graphing capabilites

--- lab1_3.py
+++ lab1_3.py
@@ -1,3 +1,4 @@
+# Some useful hint. Please feel free to program without the hint
 import tensorflow as tf
 import pandas as pd
 import numpy as np
@@ -6,33 +7,30 @@ import matplotlib.pyplot as plt
 from tensorflow.keras import layers, models, optimizers
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
-
-data=pd.read_csv('/Users/christopherandrew/Documents/CNIT 483/CNIT-483-Lab/Lab1_2.csv') # you may need to change the path
-x_data = data['x'].values
-y_data = data['y'].values
-
+# Import Data
+data=pd.read_csv('/Users/christopherandrew/Documents/CNIT 483/CNIT-483-Lab/Lab1_3.csv')
+x_data = data[['Open','High','Low']]
+y_data = data['Close']
 # Generate training data (70% of the given data samples) and the testing data (30% of the given data samples). You can change to other percentage value as long as test_size <=0.3.
 x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.3)
 
-joined = np.stack((x_train, y_train), axis=1)
-joined_test = np.stack((x_test, y_test), axis=1)
 model = models.Sequential()
-model.add(layers.Normalization(input_shape=(2,), axis=None))
+model.add(layers.Normalization(input_shape=(3,), axis=None))
 model.add(layers.Dense(1))
 model.summary()
 
-# Build learning model by using gradient-descent method
 adam = optimizers.Adam(learning_rate=0.5)
 model.compile(optimizer='adam',
               loss='mean_absolute_error',
               metrics=['mean_absolute_error'])
 
-history = model.fit(joined, y_train, epochs=3500,
-                    validation_data=(joined_test, y_test))
+history = model.fit(x_train, y_train, epochs=3500,
+                    validation_data=(x_test, y_test))
+
 W = model.layers[1].get_weights()
 print(W)
 
-test_loss, test_acc = model.evaluate(joined_test,  y_test, verbose=2)
-train_loss, train_acc = model.evaluate(joined, y_train, verbose=2)
+test_loss, test_acc = model.evaluate(x_test,  y_test, verbose=2)
+train_loss, train_acc = model.evaluate(x_train, y_train, verbose=2)
 print("Test_error: ", test_acc)
 print("Train_error: ", train_acc)
