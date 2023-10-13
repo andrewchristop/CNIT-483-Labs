a1ad795f7235f8b3ca4a93d0240616f1ab8cec04
Christopher Andrew <christopherandrew@pal-nat186-84-215.itap.purdue.edu>
2023-10-13 11:54:31 -0400

    Added lab2_3.py

---
+++ lab2_3.py
@@ -0,0 +1,27 @@
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from tensorflow.keras import layers, models, optimizers
+from sklearn import metrics
+from sklearn.metrics import classification_report
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+
+data = pd.read_csv('/Users/christopherandrew/Documents/CNIT 483/CNIT-483-Lab/Data_Lab2_2.csv')
+x_data = data[['x_data_0', 'x_data_1']]
+y_data = data['y_data']
+
+x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.3)
+
+model = LogisticRegression()
+model.fit(x_train, y_train)
+y_pred = model.predict(x_test)
+
+print(classification_report(y_test, y_pred))
+
+print("Test_error: ", 1 - model.score(x_test, y_test))
+print("Train_error: ", 1 - model.score(x_train, y_train))
+
