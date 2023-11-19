cba5c0fe1e7fd48dd3bf8eb7de20da70e0a8de23
Christopher Andrew <christopherandrew@Christophers-MacBook-Pro.local>
2023-11-18 21:32:18 -0500

    Reshaped values and generated model summary

--- lab4_1.py
+++ lab4_1.py
@@ -11,7 +11,6 @@ from sklearn.model_selection import train_test_split
 train = pd.read_csv('/Users/christopherandrew/Documents/CNIT 483/CNIT-483-Lab/Data_Lab4_1_train.csv')
 test = pd.read_csv('/Users/christopherandrew/Documents/CNIT 483/CNIT-483-Lab/Data_Lab4_1_test.csv')
 
-#x_train = train['x_train_'+str(0)].values
 x_train = np.array([])
 x_test = np.array([])
 
@@ -26,14 +25,21 @@ for j in range (0, 5):
   x_test = np.concatenate((x_test, temp_te))
 
 y_test = test['y_test'].values
-print(y_test)
+
+x_train = x_train.reshape(x_train.shape[0], 1, 1)
+x_test = x_test.reshape(x_test.shape[0], 1, 1)
+
+#print(x_train.shape)
+#print(y_train.shape)
+#print(x_test.shape)
+#print(y_test.shape)
 
 #data = np.concatenate((train, test), axis = 0)
 #print(data)
 
 #print(x_train.shape[0])
 #x_train = x_train.reshape(x_train, (x_train[0], 1, x_train.shape[1]))
-#model = models.Sequential()
-#model.add(layers.LSTM(10, input_shape=(6,)))
-#model.add(layers.Dense(1, activation='sigmoid'))
-#print(model.summary())
+model = models.Sequential()
+model.add(layers.LSTM(10, input_shape=(1,1)))
+model.add(layers.Dense(1, activation='sigmoid'))
+print(model.summary())
