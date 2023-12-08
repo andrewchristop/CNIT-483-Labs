6f524dac8bae1da92270c7600c26dbf238d9b155
Christopher Andrew <christopherandrew@Christophers-MacBook-Pro.local>
2023-12-07 22:56:06 -0500

    fixed training and testing errors and switched from binary cross entropy to mse

--- lab4_1.py
+++ lab4_1.py
@@ -11,35 +11,22 @@ from sklearn.model_selection import train_test_split
 train = pd.read_csv('/Users/christopherandrew/Documents/CNIT 483/CNIT-483-Lab/Data_Lab4_1_train.csv')
 test = pd.read_csv('/Users/christopherandrew/Documents/CNIT 483/CNIT-483-Lab/Data_Lab4_1_test.csv')
 
-x_train = np.array([])
-x_test = np.array([])
+x_train = train.drop(['y_train'], axis=1)
+x_test = test.drop(['y_test'], axis=1)
+y_train = train['y_train']
+y_test = test['y_test']
 
-for i in range(0,5):
-  temp = train['x_train_' + str(i)].values
-  x_train = np.concatenate([x_train, temp])
+x_test = x_test.values.reshape(x_test.shape[0], x_test.shape[1], 1)
+x_train = x_train.values.reshape(x_train.shape[0], x_test.shape[1], 1)
 
-y_train = train['y_train'].values
-
-for j in range (0, 5):
-  temp_te = test['x_test_' + str(j)].values
-  x_test = np.concatenate((x_test, temp_te))
-
-y_test = test['y_test'].values
-
-x_train = x_train.reshape(x_train.shape[0], 1, 1)
-x_test = x_test.reshape(x_test.shape[0], 1, 1)
-
-#print(x_train.shape)
-#print(y_train.shape)
-#print(x_test.shape)
-#print(y_test.shape)
-
-#data = np.concatenate((train, test), axis = 0)
-#print(data)
-
-#print(x_train.shape[0])
-#x_train = x_train.reshape(x_train, (x_train[0], 1, x_train.shape[1]))
 model = models.Sequential()
-model.add(layers.LSTM(10, input_shape=(1,1)))
-model.add(layers.Dense(1, activation='sigmoid'))
-print(model.summary())
+model.add(layers.LSTM(100, activation='relu', input_shape=(5, 1)))
+model.add(layers.Dense(1))
+adam = optimizers.Adam(lr=0.001)
+model.compile(optimizer='adam',loss='mse')
+model.summary()
+
+history = model.fit(x_train, y_train, epochs=200, validation_data=(x_test, y_test),verbose=1)
+
+print("Test_error: ", model.evaluate(x_test,  y_test, verbose=2))
+print("Train_error: ", model.evaluate(x_train,  y_train, verbose=2))
