8b450c29044aa0d799d75a693d58c0090efc83bd
Christopher Andrew <christopherandrew@Christophers-MacBook-Pro.local>
2023-12-08 18:27:21 -0500

    Added comments for lab4_2.py

--- lab4_2.py
+++ lab4_2.py
@@ -8,25 +8,25 @@ from tensorflow.keras import datasets, layers, models, optimizers
 from tensorflow.keras.preprocessing import sequence
 from sklearn.model_selection import train_test_split
 
-train = pd.read_csv('/Users/christopherandrew/Documents/CNIT 483/CNIT-483-Lab/Data_Lab4_1_train.csv')
-test = pd.read_csv('/Users/christopherandrew/Documents/CNIT 483/CNIT-483-Lab/Data_Lab4_1_test.csv')
+train = pd.read_csv('/Users/christopherandrew/Documents/CNIT 483/CNIT-483-Lab/Data_Lab4_1_train.csv') #Import training data and saving it to train numpy array
+test = pd.read_csv('/Users/christopherandrew/Documents/CNIT 483/CNIT-483-Lab/Data_Lab4_1_test.csv') #Import testing data and saving it to test numpy array
 
-x_train = train.drop(['y_train'], axis=1)
-x_test = test.drop(['y_test'], axis=1)
-y_train = train['y_train']
-y_test = test['y_test']
+x_train = train.drop(['y_train'], axis=1) #Dropping y_train column from train array to exclusively save x_train data
+x_test = test.drop(['y_test'], axis=1) #Dropping y_test column from test array to exclusively save x_test data
+y_train = train['y_train'] #Saving y_train column from train array to y_train array
+y_test = test['y_test'] #Saving y_test column from test array to y_test array
 
-x_test = x_test.values.reshape(x_test.shape[0], x_test.shape[1], 1)
-x_train = x_train.values.reshape(x_train.shape[0], x_test.shape[1], 1)
+x_test = x_test.values.reshape(x_test.shape[0], x_test.shape[1], 1) #Reshaping data 
+x_train = x_train.values.reshape(x_train.shape[0], x_test.shape[1], 1) #Reshaping data 
 
 model = models.Sequential()
-model.add(layers.GRU(64, activation='relu' ,input_shape=(5,1)))
-model.add(layers.Dense(1))
-adam = optimizers.Adam(lr=0.001)
-model.compile(optimizer='adam', loss='mse')
-model.summary()
+model.add(layers.GRU(64, activation='relu' ,input_shape=(5,1))) #Importing GRU layer with 64 computing nodes, using relu activation function
+model.add(layers.Dense(1)) #Dense layer
+adam = optimizers.Adam(lr=0.001) #Using adam optimizer with a learning rate of 0.001
+model.compile(optimizer='adam', loss='mse') #Model compilation using adam optimizer and mean squared error loss function
+model.summary() #Prints model summary
 
-history = model.fit(x_train, y_train, validation_data=(x_test, y_test), batch_size=32, epochs=100)
+history = model.fit(x_train, y_train, validation_data=(x_test, y_test), batch_size=32, epochs=100) #Running the model at 100 epochs
 
-print("Test_error: ", model.evaluate(x_test,  y_test, verbose=2))
-print("Train_error: ", model.evaluate(x_train,  y_train, verbose=2))
+print("Test_error: ", model.evaluate(x_test,  y_test, verbose=2)) #Printing training error
+print("Train_error: ", model.evaluate(x_train,  y_train, verbose=2)) #Printing testing error
